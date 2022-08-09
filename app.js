1f359434613e618456ec04235c45523473905f94
muhammad-hamza <Muh-Hamza-99>
2022-08-09 18:14:48 +0300

    Refactoring and re-moving code to get user's input

--- app.js
+++ app.js
@@ -1,136 +1,8 @@
-const inquirer = require("inquirer");
-
 const chalk = require("chalk");
 
+const getUserInput = require("./utilities/get-user-input");
+
 const fileExists = require("./utilities/file-exists");
-
-const THEMES = require("./lib/themes");
-const FONTS = require("./lib/fonts");
-
-const getUserInput = async () => {
-    const inputs = await inquirer.prompt([
-        {
-            type: "list",
-            name: "t",
-            message: "Syntax Theme",
-            choices: Object.entries(THEMES).map(theme => theme[0]),
-            default: "Duotone",
-        },
-        {
-            type: "input",
-            name: "bg",
-            message: "Background Color",
-            validate: async input => {
-                if (!/^#(?:[0-9a-fA-F]{3}){1,2}$/.test(input)) return "Color must be a valid hexadecimal code.";
-                return true;
-            },
-            default: "#333333",
-        },
-        {
-            type: "list",
-            name: "fm",
-            choices: Object.entries(FONTS).map(theme => theme[0]),
-            message: "Font Family",
-            default: "Fantasque Sans Mono",
-        },
-        {
-            type: "input",
-            name: "fs",
-            message: "Font Size",
-            validate: async input => {
-                if (!input.endsWith("px")) return "Value must end with 'px'";
-                return true;
-            },
-            default: "18px",
-        },
-        {
-            type: "input",
-            name: "lh",
-            message: "Line Height",
-            validate: async input => {
-                if (!input.endsWith("%")) return "Value must end with '%'";
-                return true;
-            },
-            default: "133%",
-        },
-        {
-            type: "input",
-            name: "pv",
-            message: "Vertical Padding?",
-            validate: async input => {
-                if (!input.endsWith("px")) return "Value must end with 'px'";
-                return true;
-            },
-            default: "50px"
-        },
-        {
-            type: "input",
-            name: "ph",
-            message: "Horizontal Padding?",
-            validate: async input => {
-                if (!input.endsWith("px")) return "Value must end with 'px'";
-                return true;
-            },
-            default: "60px"
-        },
-        {
-            type: "confirm",
-            name: "ln",
-            message: "Include Line Numbers?",
-            default: false,
-        },
-        {
-            type: "confirm",
-            name: "wc",
-            message: "Include Window Controls?",
-            default: true,
-        },
-        {
-            type: "list",
-            name: "wt",
-            message: "Window Theme?",
-            choices: ["none", "sharp", "boxy", "bw"],
-            default: "none",
-            when: answers => answers.wc
-        },
-        {
-            type: "confirm",
-            name: "ds",
-            message: "Include Drop Shadow?",
-            default: false
-        },
-        {
-            type: "input",
-            name: "dsyoff",
-            message: "Drop Shadow Y-Offset?",
-            validate: async input => {
-                if (!input.endsWith("px")) return "Value must end with 'px'";
-                return true;
-            },
-            default: "20px",
-            when: answers => answers.ds
-        },
-        {
-            type: "input",
-            name: "dsblur",
-            message: "Drop Shadow Blur",
-            validate: async input => {
-                if (!input.endsWith("px")) return "Value must end with 'px'";
-                return true;
-            },
-            default: "68px",
-            when: answers => answers.ds
-        },
-        {
-            type: "list",
-            name: "exportAsFiletype",
-            message: "Export As",
-            choices: ["svg", "png"],
-            default: "png",
-        },
-    ]);
-    console.log(inputs);
-};
 
 const file = process.argv[2];
 if (!file) {
@@ -143,4 +15,9 @@ if (!fileExists(file)) {
     process.exit(1);
 };
 
-getUserInput();
+const main = async () => {
+    const inputs = await getUserInput();
+    console.log(inputs);
+};
+
+main();
---
+++ utilities/get-user-input.js
@@ -0,0 +1,131 @@
+const inquirer = require("inquirer");
+
+const THEMES = require("../lib/themes");
+const FONTS = require("../lib/fonts");
+
+const getUserInput = async () => {
+    const inputs = await inquirer.prompt([
+        {
+            type: "list",
+            name: "t",
+            message: "Syntax Theme",
+            choices: Object.entries(THEMES).map(theme => theme[0]),
+            default: "Duotone",
+        },
+        {
+            type: "input",
+            name: "bg",
+            message: "Background Color",
+            validate: async input => {
+                if (!/^#(?:[0-9a-fA-F]{3}){1,2}$/.test(input)) return "Color must be a valid hexadecimal code.";
+                return true;
+            },
+            default: "#333333",
+        },
+        {
+            type: "list",
+            name: "fm",
+            choices: Object.entries(FONTS).map(theme => theme[0]),
+            message: "Font Family",
+            default: "Fantasque Sans Mono",
+        },
+        {
+            type: "input",
+            name: "fs",
+            message: "Font Size",
+            validate: async input => {
+                if (!input.endsWith("px")) return "Value must end with 'px'";
+                return true;
+            },
+            default: "18px",
+        },
+        {
+            type: "input",
+            name: "lh",
+            message: "Line Height",
+            validate: async input => {
+                if (!input.endsWith("%")) return "Value must end with '%'";
+                return true;
+            },
+            default: "133%",
+        },
+        {
+            type: "input",
+            name: "pv",
+            message: "Vertical Padding?",
+            validate: async input => {
+                if (!input.endsWith("px")) return "Value must end with 'px'";
+                return true;
+            },
+            default: "50px"
+        },
+        {
+            type: "input",
+            name: "ph",
+            message: "Horizontal Padding?",
+            validate: async input => {
+                if (!input.endsWith("px")) return "Value must end with 'px'";
+                return true;
+            },
+            default: "60px"
+        },
+        {
+            type: "confirm",
+            name: "ln",
+            message: "Include Line Numbers?",
+            default: false,
+        },
+        {
+            type: "confirm",
+            name: "wc",
+            message: "Include Window Controls?",
+            default: true,
+        },
+        {
+            type: "list",
+            name: "wt",
+            message: "Window Theme?",
+            choices: ["none", "sharp", "boxy", "bw"],
+            default: "none",
+            when: answers => answers.wc
+        },
+        {
+            type: "confirm",
+            name: "ds",
+            message: "Include Drop Shadow?",
+            default: false
+        },
+        {
+            type: "input",
+            name: "dsyoff",
+            message: "Drop Shadow Y-Offset?",
+            validate: async input => {
+                if (!input.endsWith("px")) return "Value must end with 'px'";
+                return true;
+            },
+            default: "20px",
+            when: answers => answers.ds
+        },
+        {
+            type: "input",
+            name: "dsblur",
+            message: "Drop Shadow Blur",
+            validate: async input => {
+                if (!input.endsWith("px")) return "Value must end with 'px'";
+                return true;
+            },
+            default: "68px",
+            when: answers => answers.ds
+        },
+        {
+            type: "list",
+            name: "exportAsFiletype",
+            message: "Export As",
+            choices: ["svg", "png"],
+            default: "png",
+        },
+    ]);
+    return inputs;
+};
+
+module.exports = getUserInput;
